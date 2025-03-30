32cd8d991c3ab0d93470417269490a31f56de7e8
Salatiel Queiroz <6747627+salatielsql@users.noreply.github.com>
2025-03-30 18:49:46 -0300

    chore: setup simple pnpm workspaces

---
+++ apps/web/index.js
@@ -0,0 +1,7 @@
+import { common } from 'common'
+import { shared } from 'shared'
+
+console.log('-------')
+console.log(shared())
+console.log(common())
+console.log('-------')
---
+++ packages/common/index.js
@@ -0,0 +1,6 @@
+import { shared } from 'shared'
+
+export const common = () => {
+  console.log(shared())
+  return 'common'
+}
